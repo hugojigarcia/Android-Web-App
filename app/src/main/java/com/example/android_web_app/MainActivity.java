4d3b421ca3295ae5e4fdc6468a9ebd84e3e1ec39
hugojigarcia <hugojimenezgarcia.hjg@gmail.com>
2022-08-17 18:52:01 +0200

    Create User Interface

--- app/src/main/java/com/example/android_web_app/MainActivity.java
+++ app/src/main/java/com/example/android_web_app/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.android_web_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView pagina;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
     }
+
+    private void initComponents() {
+        ocultarBarra();
+        this.url = getUrl();
+        initWeb();
+    }
+
+    private void ocultarBarra(){
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+
+    private String getUrl(){
+        //TODO
+        return "";
+    }
+
+    private void initWeb(){
+        pagina = (WebView) findViewById(R.id.pagina);
+        pagina.setWebViewClient(new WebViewClient());
+        pagina.getSettings().setJavaScriptEnabled(true);
+        cargarWeb();
+    }
+
+    private void cargarWeb(){
+        pagina.loadUrl(url);
+    }
+
+
 }
